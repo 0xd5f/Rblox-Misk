3de11986f5761177acf7dcd2cf55d584869e7fb1
Alex <79174725+0xd5f@users.noreply.github.com>
2022-07-20 02:16:27 +0400

    Update RecoilBeta.lua

--- RecoilBeta.lua
+++ RecoilBeta.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0xd5f/Rblox-Misk/main/Loaders.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0xd5f/Rblox-Misk/main/Loader.lua", true))()
